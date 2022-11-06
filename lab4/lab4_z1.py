a100d7cca51e49bfa63571a1ec482d97a211eb59
w67131 <w67131@student.wsiz.edu.pl>
2022-11-06 14:07:56 +0100

    koniec zajęć

---
+++ lab4/lab4_z1.py
@@ -0,0 +1,10 @@
+
+lista_zakupów=["Mleko","Ser","Masło","Jajka"]
+
+# a)
+
+print(lista_zakupów)
+print(lista_zakupów[0]+", "+lista_zakupów[1])
+lista_zakupów[2]="kot"
+print(lista_zakupów)
+print(lista_zakupów[-2])
