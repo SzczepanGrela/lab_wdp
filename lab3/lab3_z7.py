c98f6cdf016b019ddf94c10b80e9b558df16ee1c
w67131 <w67131@student.wsiz.edu.pl>
2022-11-06 13:10:18 +0100

    proba lab 3

---
+++ lab3/lab3_z7.py
@@ -0,0 +1,19 @@
+# a)
+for ciag in range(1,101):
+    print(ciag, end=' ')
+print("\n")
+
+# b)
+for ciag2 in range(100,0,-1):
+    print(ciag2,end=' ')
+print("\n")
+
+# c)
+for ciag3 in range(7,78,7):
+    print(ciag3,end=' ')
+print("\n")
+
+# d)
+for ciag4 in range(20,-1,-2):
+    print(ciag4,end=' ')
+print("\n")
--- lab_3.py
+++ lab_3.py
@@ -1,2 +1,23 @@
-x=5
+
+# zad 1
+
+
+x=(int(input("podaj liczbę 1: ")))
+
+y=(int(input("podaj liczbę 2: ")))
+
+
+if x>y:
+    wi=x
+    mn=y
+else:
+    wi=y
+    mn=x
+while mn<=wi:
+    print(mn)
+    mn=mn+1
+
+
+
+
 
