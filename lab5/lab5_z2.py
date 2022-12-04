3f9d1746eb3d88649bb19d0ac4651b1690e10f58
w67131 <w67131@student.wsiz.edu.pl>
2022-12-04 14:18:10 +0100

    koniec lab 5

---
+++ lab5/lab5_z2.py
@@ -0,0 +1,24 @@
+
+
+
+sample_dict = {"name":"Kelly","surname":"Jones","age":25,"salary":8000,"city":"New York"}
+
+
+#for k in sample_dict.keys():
+ #   print(f' {k:<10}  = {sample_dict[k]:>10}')
+
+#for k,v in sample_dict.items():
+ #   print(f' {k:<10}  = {v:>10}')
+
+L = {'age','name','house'}
+sample_dict2={}
+for i in L:
+
+        print(sample_dict.pop(i,'Brak pary o danym kluczu'))
+
+
+
+
+#sample_dict2 = {i: sample_dict[i] for i in L if i in sample_dict}
+#print(sample_dict2)
+
